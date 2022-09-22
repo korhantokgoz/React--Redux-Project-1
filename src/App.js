07dd92a029838cb729bb6c129e8368becadcd53b
korhantokgoz <korhan.tokgoz92@gmail.com>
2022-09-22 12:29:39 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import ProductListing from "./container/ProductListing";
+import Header from "./container/Header";
+import "./App.css";
+import ProductDetails from "./container/ProductDetail";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Routes>
+        <Header />
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route>404 Not Found!</Route>
+          </Routes>
+      </Router>
     </div>
   );
 }
